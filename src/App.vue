034f0655b389085c8af00f7b9c2ef2711f074a9e
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-03-09 11:01:55 +0100

    feat: add Header component and remove unused HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="c-app" id="app">
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Header } from "./components";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    Header,
   }
 });
 </script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="c-header">
+    <h1 class="c-header__title">Bike Spaces <small>(v1.0.0)</small></h1>
+    <em class="c-header__powered">
+      <span>Powered by</span>
+      <img src="@/assets/logo.png" alt="Vue logo" />
+    </em>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.c-header {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+}
+
+.c-header__title {
+  margin: 0;
+  font-size: 2rem;
+  color: #263238;
+
+  small {
+    font-size: 1rem;
+    color: #546e7a;
+  }
+}
+
+.c-header__powered {
+  display: flex;
+  align-items: center;
+
+  span {
+    position: absolute;
+    white-space: nowrap;
+    opacity: 0;
+    transform: translateX(-75%);
+    transition-property: opacity, transform;
+    transition-duration: 300ms;
+    transition-timing-function: ease-in-out;
+  }
+
+  img {
+    height: 2rem;
+    margin-left: 0.5rem;
+  }
+
+  &:hover {
+    span {
+      opacity: 1;
+      transform: translateX(-100%);
+    }
+  }
+}
+</style>
